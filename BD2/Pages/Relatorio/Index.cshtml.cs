18f8ac839580fca649762f58e37fedde72b97df1
Talita Pacheco <80596580+Talitapach@users.noreply.github.com>
2023-11-02 19:15:20 -0300

    Update Relatorio

--- BD2/Pages/Relatorio/Index.cshtml.cs
+++ BD2/Pages/Relatorio/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using BD2.Pages.Clientes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Win32;
@@ -8,34 +9,56 @@ using System.Xml;
 namespace BD2.Pages.Relatorio
 {
     public class IndexModel : PageModel
-    { 
-        public DataTable ProcedureRelatorio()
     {
-        var dt = new DataTable();
+        List<Relatorio> Relatorios = new List<Relatorio>();
+
+        public void ProcedureRelatorio() 
+        {
 
         try
         {
             String connectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=Northwind;Integrated Security=True";
 
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-
-                using (SqlDataAdapter sqlCommand = new SqlDataAdapter(null, sqlConnection))
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
                 {
-                    sqlCommand.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
-                    sqlCommand.SelectCommand.CommandText = "GetCategorySalesReport";
+                    sqlConnection.Open();
 
-                    sqlCommand.Fill(dt);
+                    String sql = "EXECUTE GetCategorySalesReport";
+                    using (SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection))
+                    {
+                        using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Relatorio relatorio = new Relatorio();
+                                relatorio.Categoria = "" + reader.GetString(0);
+                                relatorio.QuantidadeVendida = "" + reader.GetInt32(1);
+                                relatorio.TotalDasVendas = "" + reader.GetSqlDouble(2);
+                                relatorio.MaiorComprador = "" + reader.GetString(3);
+                                relatorio.PaísComMaisCompras = "" + reader.GetString(4);
+                                relatorio.CidadeComMaisCompras = "" + reader.GetString(5);
+
+                                Relatorios.Add(relatorio);
+                            }
+                        }
+                    }
                 }
-            }
         }
         catch (Exception ex)
         {
             Console.WriteLine("Exception:  " + ex.ToString());
         }
-        return dt;
+
     }
+
 }
-    
+    public class Relatorio
+    {
+        public String Categoria;
+        public String QuantidadeVendida;
+        public String TotalDasVendas;
+        public String MaiorComprador;
+        public String PaísComMaisCompras;
+        public String CidadeComMaisCompras;
+    }
 }
